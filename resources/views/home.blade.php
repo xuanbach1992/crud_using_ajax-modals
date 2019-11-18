beedf1d40fcc3d54f27e97f1885cfe844287e5fa
xuanbach1992 <Xuanbach1992>
2019-11-18 08:57:16 +0700

    edit route home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -141,7 +141,7 @@
     </div>
     <div class="row">
         <nav class="navbar navbar-expand-lg col-lg-10  navbar-light bg-light">
-            <a class="navbar-brand" href="#">Home</a>
+            <a class="navbar-brand" href="{{route('home')}}">Home</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("home");
 Route::prefix("customers")->group(function () {
     Route::get('/', "CustomerController@show")->name('customers.list');
     Route::get("/search", "CustomerController@search")->name("customer.search");
